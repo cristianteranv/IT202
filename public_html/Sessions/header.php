16f1123580d7f1079133d43267b27e63702a3881
Cristian Teran <cristianteranv@gmail.com>
2020-06-10 18:07:00 -0400

    Messing with styling

--- public_html/Sessions/header.php
+++ public_html/Sessions/header.php
@@ -1,6 +1,11 @@
 <head>
+    <title>A title</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+<?php
+require("config.php");
+session_start();
+?>
 <nav>
     <ul>
         <li><a href="home.php">Home</a></li>
@@ -9,7 +14,3 @@
         <li><a href="logout.php">Logout</a></li>
     </ul>
 </nav>
-<?php
-    require("config.php");
-    session_start();
-?>
