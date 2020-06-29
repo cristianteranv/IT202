5c37a367c014a7c1dfae54f91fd2970c63fa02f8
Cristian Teran <cristianteranv@gmail.com>
2020-06-28 23:01:08 -0400

    Added week6 directory and files. Edited.

--- public_html/week6/search.php
+++ public_html/week6/search.php
@@ -1,29 +1,36 @@
 <?php
 $search = "";
+$sort = "";
 if(isset($_POST["search"])){
     $search = $_POST["search"];
+}
+if(isset($_POST["sort"])){
+    $sort = $_POST["sort"];
 }
 ?>
     <form method="POST">
         <input type="text" name="search" placeholder="Search for product"
                value="<?php echo $search;?>"/>
+        <input type="radio" name="sort" id="asc" value="asc"/>
+        <label for="asc">Ascending</label>
+        <input type="radio" name="sort" id="desc" value="desc"/>
+        <label for="desc">Descending</label>
         <input type="submit" value="Search"/>
     </form>
 <?php
 if(isset($search)) {
 
     require("common.inc.php");
-    $query = file_get_contents(__DIR__ . "/queries/SEARCH_TABLE_THINGS.sql");
-    if (isset($query) && !empty($query)) {
-        try {
-            $stmt = getDB()->prepare($query);
-            //Note: With a LIKE query, we must pass the % during the mapping
-            $stmt->execute([":thing"=>$search]);
-            //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
+    $query = "SELECT * FROM Products WHERE name LIKE :product";
+    echo "<div>Sort has this value: " . $sort . "</div>";
+    try {
+        $stmt = getDB()->prepare($query);
+        //Note: With a LIKE query, we must pass the % during the mapping
+        $stmt->execute([":product"=>$search]);
+        //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
 }
 ?>
@@ -31,18 +38,18 @@ if(isset($search)) {
     note the structure and the ":" -->
     <!-- note how we must close each check we're doing as well-->
 <?php if(isset($results) && count($results) > 0):?>
-    <p>This shows when we have results</p>
+    <p>Here are your search results.</p>
     <ul>
         <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
         we're also using our helper function to safely return a value based on our key/column name.-->
         <?php foreach($results as $row):?>
             <li>
-                <?php echo get($row, "name")?>
-                <?php echo get($row, "quantity");?>
+                <?php echo get($row, "id")?>
+                <?php echo get($row, "name");?>
                 <a href="delete.php?thingId=<?php echo get($row, "id");?>">Delete</a>
             </li>
         <?php endforeach;?>
     </ul>
 <?php else:?>
-    <p>This shows when we don't have results</p>
+    <p>There are no products matching your search.</p>
 <?php endif;?>
