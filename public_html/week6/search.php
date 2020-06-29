87c2c53503640d6be8b375033e69b755c494235c
Cristian Teran <cristianteranv@gmail.com>
2020-06-28 22:33:33 -0400

    Added week6 directory and files. Edited.

--- public_html/week6/search.php
+++ public_html/week6/search.php
@@ -3,40 +3,27 @@ $search = "";
 if(isset($_POST["search"])){
     $search = $_POST["search"];
 }
+if(isset($_POST["sort"])){
+    $sort = $_POST["sort"];
+}
 ?>
 
 <?php
 if(isset($search)) {
     require("common.inc.php");
     try {
-        $sort = $_POST["sort"];
         echo "This is the value of sort: " . $sort . "<br>";
         $stmt = getDB()->prepare("SELECT * FROM Products WHERE name LIKE CONCAT('%', :product, '%')");
         //Note: With a LIKE query, we must pass the % during the mapping
         $stmt->execute([":product"=>$search]);
         //Note the fetchAll(), we need to use it over fetch() if we expect >1 record
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results["status"] = 200;
+        $results["results"] = $results;
     } catch (Exception $e) {
-        echo $e->getMessage();
+        $results["status"] = 400;
+        $results["error"] = $e->getMessage();
     }
+    echo json_encode($results);
 }
 ?>
-    <!--This part will introduce us to PHP templating,
-    note the structure and the ":" -->
-    <!-- note how we must close each check we're doing as well-->
-<?php if(isset($results) && count($results) > 0):?>
-    <p>Here are the search results.</p>
-    <ul>
-        <!-- Here we'll loop over all our results and reuse a specific template for each iteration,
-        we're also using our helper function to safely return a value based on our key/column name.-->
-        <?php foreach($results as $row):?>
-            <li>
-                <?php echo get($row, "id")?>
-                <?php echo get($row, "name");?>
-                <a href="delete.php?productId=<?php echo get($row, "id");?>">Delete</a>
-            </li>
-        <?php endforeach;?>
-    </ul>
-<?php else:?>
-    <p>No product names match your search.</p>
-<?php endif;?>
