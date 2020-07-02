3ca29e420b9b339ba81ba0342ebc7b42484e142d
Cristian Teran <cristianteranv@gmail.com>
2020-07-02 14:57:23 -0400

    Added create, edit, and search (which includes deletion) links to header.php when user is logged in

--- public_html/Project/header.php
+++ public_html/Project/header.php
@@ -9,12 +9,17 @@ session_start();
     <ul>
         <li><a href="home.php">Home</a></li>
         <li><a href="register.php">Register</a></li>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="logout.php">Logout</a></li>
         <?php
         if (isset($_SESSION["user"])){
             echo "<li><a href='create.php'>Create new product</a> </li>";
+            echo "<li><a href='edit.php'>Edit product</a> </li>";
+            echo "<li><a href='search.php'>Delete product</a> </li>";
+
+        }
+        else{
+            echo "<li><a href='login.php'>Login</a></li>";
         }
         ?>
+        <li><a href="logout.php">Logout</a></li>
     </ul>
 </nav>
