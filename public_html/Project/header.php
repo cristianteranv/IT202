e90cae81d51d0632a5d514ebae1e85e799b5bcab
Cristian Teran <cristianteranv@gmail.com>
2020-07-02 14:09:50 -0400

    Added directory for project.

--- public_html/Project/header.php
+++ public_html/Project/header.php
@@ -11,5 +11,10 @@ session_start();
         <li><a href="register.php">Register</a></li>
         <li><a href="login.php">Login</a></li>
         <li><a href="logout.php">Logout</a></li>
+        <?php
+        if (isset($_SESSION["user"])){
+            echo "<li><a href='create.php'>Create new product</a> </li>";
+        }
+        ?>
     </ul>
 </nav>
