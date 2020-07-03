55b688ef5a6fea9f572ec18f9a53e3433958d5a7
Cristian Teran <cristianteranv@gmail.com>
2020-07-03 15:14:10 -0400

    Added header styling

--- public_html/Project/header.php
+++ public_html/Project/header.php
@@ -4,19 +4,23 @@
 <?php
 session_start();
 ?>
-<nav>
-    <ul>
-        <li><a href="home.php">Home</a></li>
-        <?php
-        if (isset($_SESSION["user"])){
-            echo "<li><a href='create.php'>Create new product</a> </li>";
-            echo "<li><a href='search.php'>Search product</a> </li>";
-            echo "<li><a href='logout.php'>Logout</a></li>";
-        }
-        else{
-            echo "<li><a href='login.php'>Login</a></li>";
-            echo "<li><a href='register.php'>Register</a></li>";
-        }
-        ?>
-    </ul>
-</nav>
+<header>
+    <nav>
+        <div id="menu">
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <?php
+                if (isset($_SESSION["user"])){
+                    echo "<li><a href='create.php'>Create new product</a> </li>";
+                    echo "<li><a href='search.php'>Search product</a> </li>";
+                    echo "<li><a href='logout.php'>Logout</a></li>";
+                }
+                else{
+                    echo "<li><a href='login.php'>Login</a></li>";
+                    echo "<li><a href='register.php'>Register</a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </nav>
+</header>
