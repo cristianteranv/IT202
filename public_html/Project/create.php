62b10f9e1d4b3a871bece5d1b5a1fede2e0e5538
Cristian Teran <cristianteranv@gmail.com>
2020-07-03 01:47:57 -0400

    replaced input text element to a textarea element for product description.

--- public_html/Project/create.php
+++ public_html/Project/create.php
@@ -18,13 +18,6 @@ $db = getDB();
     textarea{
         height: 100px;
         vertical-align: middle;
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
     }
 </style>
 <form method="POST">
