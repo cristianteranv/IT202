6485c1680eb37886a91b483a456b5d8ccfd6589a
Cristian Teran <cristianteranv@gmail.com>
2020-07-03 00:34:35 -0400

    changed justify-all to justify

--- public_html/Project/header.php
+++ public_html/Project/header.php
@@ -7,17 +7,16 @@ session_start();
 <nav>
     <ul>
         <li><a href="home.php">Home</a></li>
-        <li><a href="register.php">Register</a></li>
         <?php
         if (isset($_SESSION["user"])){
             echo "<li><a href='create.php'>Create new product</a> </li>";
-            echo "<li><a href='edit.php'>Edit product</a> </li>";
-            echo "<li><a href='search.php'>Delete product</a> </li>";
+            echo "<li><a href='search.php'>Search product</a> </li>";
+            echo "<li><a href='logout.php'>Logout</a></li>";
         }
         else{
             echo "<li><a href='login.php'>Login</a></li>";
+            echo "<li><a href='register.php'>Register</a></li>";
         }
         ?>
-        <li><a href="logout.php">Logout</a></li>
     </ul>
 </nav>
