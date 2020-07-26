314eb7ea88e2af6a0508ea1fa817cb4bd0300010
Cristian Teran <cristianteranv@gmail.com>
2020-07-26 16:43:05 -0400

    Fixing query results for home.php

--- public_html/Project/myjs.js
+++ public_html/Project/myjs.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('form.cartForm').click(function () {
-        $.post("addToCart.php", function (data, status) {
-            alert("Data: " + data + "\nStatus: " + status);
+        $.post("addToCart.php", function (data) {
+            alert("Data: " + data);
         });
     });
 });
