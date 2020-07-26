23af312b7218aa2dd8e14e1b23572f1c59def769
Cristian Teran <cristianteranv@gmail.com>
2020-07-26 15:57:44 -0400

    Fixing query results for home.php

--- public_html/Project/myjs.js
+++ public_html/Project/myjs.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-    $('form.subCart').click(function () {
-
-        alert("hi");
-        return false;
-    })
-
-    /*$('form.cartForm').click(function () {
+    $('form.cartForm').click(function () {
         alert("hi2");
         return false;
-    })*/
+    })
 });
