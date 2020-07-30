ecb1667658a45d80a944e6c23e2396bf2193804f
Cristian Teran <cristianteranv@gmail.com>
2020-07-30 18:45:50 -0400

    added li.itemHeader to style.css

--- public_html/Project/addToCart.php
+++ public_html/Project/addToCart.php
@@ -3,5 +3,5 @@
     $userId = $_POST["userId"];
     $productId = $_POST["productId"];
     $quantity = $_POST["purchaseQuantity"];
-    echo $price . $userId. $productId . $quantity;
+    echo "Price: " . $price . "\nUserId: " . $userId. "\nProductId: " . $productId . "\nQuantitiy: " .$quantity;
 ?>
