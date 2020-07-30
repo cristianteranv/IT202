fed5f63fc3eece741067f178700faa38edff3d3e
Cristian Teran <cristianteranv@gmail.com>
2020-07-30 16:12:05 -0400

    Fixing query results for home.php

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -106,7 +106,7 @@ note the structure and the ":" -->
                     <input type="number" name="price" value="<?php echo get($row, "price")?>" hidden>
                     <input type="number" name="userId" value="<?php echo $_SESSION["user"]["id"]?>" hidden>
                     <input type="number" name="productId" value="<?php echo get($row, "id")?>" hidden>
-                    <input type="number" name="purchaseQuantity" value="1" style="width: 50%">
+                    <input type="number" name="purchaseQuantity" value="1" style="width: 100%">
                     <input type="submit" id="subCart" class="subCart" value="Add to cart" style="width: 100%; padding: 3px; margin: 2px;">
                 </form>
             </li>
