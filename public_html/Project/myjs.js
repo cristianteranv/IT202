a9c16baf9bb57b9363722e0052a4045e6af1fc0e
Cristian Teran <cristianteranv@gmail.com>
2020-07-31 18:49:24 -0400

    Implementing adding to cart functionality

--- public_html/Project/myjs.js
+++ public_html/Project/myjs.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('form.cartForm').submit(function () {
         var formValues = $(this).serialize();
+        alert(formValues);
         $.post("addToCart.php", formValues,
             function (data) {
             alert("Data: " + data);
