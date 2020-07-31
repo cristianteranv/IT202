16e195f89ee54db9b41301a831016d87aa180796
Cristian Teran <cristianteranv@gmail.com>
2020-07-31 18:22:32 -0400

    Added Cart or store reference to header menu

--- public_html/Project/addToCart.php
+++ public_html/Project/addToCart.php
@@ -2,11 +2,30 @@
     include("header.php");
     require("common.inc.php");
     $db = getDB();
-    $price = $_POST["price"];
     $userId = $_POST["userId"];
     $productId = $_POST["productId"];
     $quantity = $_POST["purchaseQuantity"];
+    $price = $_POST["price"];
     if (!empty($userId) && !empty($price) && !empty($productId) && !empty($quantity)){
-        $stmt = $db->prepare("INSERT INTO Carts VALUES")
+        $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
+        $result = $stmt->execute(array(
+            ":userId" => $userId,
+            ":productId" => $productId,
+            ":quantity" => $quantity,
+            ":price" => $price
+        ));
+        $e = $stmt->errorInfo();
+        if ($e[0] != "00000") {
+            echo var_export($e, true);
+        }
+        else {
+            echo var_export($result, true);
+            if ($result) {
+                echo "Successfully added to cart!";
+            }
+            else {
+                echo "Error while adding to cart";
+            }
+        }
     }
 ?>
