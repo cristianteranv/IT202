022ed657584def1727a758fb0f27bdefea82dc48
Cristian Teran <cristianteranv@gmail.com>
2020-07-31 18:34:25 -0400

    Implementing adding to cart functionality

--- public_html/Project/addToCart.php
+++ public_html/Project/addToCart.php
@@ -2,30 +2,34 @@
     include("header.php");
     require("common.inc.php");
     $db = getDB();
-    $userId = $_POST["userId"];
-    $productId = $_POST["productId"];
-    $quantity = $_POST["purchaseQuantity"];
-    $price = $_POST["price"];
-    if (!empty($userId) && !empty($price) && !empty($productId) && !empty($quantity)){
-        $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
-        $result = $stmt->execute(array(
-            ":userId" => $userId,
-            ":productId" => $productId,
-            ":quantity" => $quantity,
-            ":price" => $price
-        ));
-        $e = $stmt->errorInfo();
-        if ($e[0] != "00000") {
-            echo var_export($e, true);
-        }
-        else {
-            echo var_export($result, true);
-            if ($result) {
-                echo "Successfully added to cart!";
+    if (isset($_POST["userId"]) && isset($_POST["productId"]) && isset($_POST["purchaseQuantity"]) && isset($_POST["price"])) {
+        try {
+            $userId = $_POST["userId"];
+            $productId = $_POST["productId"];
+            $quantity = $_POST["purchaseQuantity"];
+            $price = $_POST["price"];
+            $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
+            $result = $stmt->execute(array(
+                ":userId" => $userId,
+                ":productId" => $productId,
+                ":quantity" => $quantity,
+                ":price" => $price
+            ));
+            $e = $stmt->errorInfo();
+            if ($e[0] != "00000") {
+                echo var_export($e, true);
             }
             else {
-                echo "Error while adding to cart";
+                echo var_export($result, true);
+                if ($result) {
+                    echo "Successfully added to cart!";
+                } else {
+                    echo "Error while adding to cart";
+                }
             }
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
         }
     }
 ?>
