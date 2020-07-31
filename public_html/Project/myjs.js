49bc7a39c12fe83849825cec22c3b2e604fcb78d
Cristian Teran <cristianteranv@gmail.com>
2020-07-31 19:31:13 -0400

    Implementing adding to cart functionality

--- public_html/Project/myjs.js
+++ public_html/Project/myjs.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     $('form.cartForm').submit(function () {
+
         var formValues = $(this).serialize();
         if(formValues.includes("login=false")){
             alert("You must be logged in!");
-            window.location.replace("https://it202-450.herokuapp.com/public_html/Project/login.php");
+            window.location = "https://it202-450.herokuapp.com/public_html/Project/login.php");
         }
         else {
             $.post("addToCart.php", formValues,
