cda2137d534a48f76c262730421f6381bdf44e28
Cristian Teran <cristianteranv@gmail.com>
2020-07-31 22:55:38 -0400

    Implementing adding to cart functionality

--- public_html/Project/addToCart.php
+++ public_html/Project/addToCart.php
@@ -3,16 +3,33 @@
     require("common.inc.php");
     $db = getDB();
     if (isset($_POST["userId"]) && isset($_POST["productId"]) && isset($_POST["purchaseQuantity"]) && isset($_POST["price"])) {
-        $userId = $_POST["userId"];
-        $productId = $_POST["productId"];
-        $quantity = $_POST["purchaseQuantity"];
-        $price = $_POST["price"];
-        $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
-        $result = $stmt->execute(array(
-            ":userId" => $userId,
-            ":productId" => $productId,
-            ":quantity" => $quantity,
-            ":price" => $price
-        ));
+        try {
+            $userId = $_POST["userId"];
+            $productId = $_POST["productId"];
+            $quantity = $_POST["purchaseQuantity"];
+            $price = $_POST["price"];
+            $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
+            $result = $stmt->execute(array(
+                ":userId" => $userId,
+                ":productId" => $productId,
+                ":quantity" => $quantity,
+                ":price" => $price
+            ));
+            $e = $stmt->errorInfo();
+            if ($e[0] != "00000") {
+                echo var_export($e, true);
+            }
+            else {
+                echo var_export($result, true);
+                if ($result) {
+                    echo "Successfully added to cart!";
+                } else {
+                    echo "Error while adding to cart";
+                }
+            }
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
     }
 ?>
--- public_html/Project/myjs.js
+++ public_html/Project/myjs.js
@@ -1,19 +1,15 @@
 $(document).ready(function(){
     $('form.cartForm').submit(function () {
         var formValues = $(this).serialize();
-        if(formValues.includes("login=false")){
-            alert("You must be logged in!");
-            window.location = "https://it202-450.herokuapp.com/public_html/Project/login.php";
-            return false;
-        }
-        else {
-            $.post(
-                "addToCart.php",
-                formValues,
-                function (data) {
-                    alert("Data: " + data);
-                }
-            );
-        }
+
+
+        $.post(
+            "addToCart.php",
+            formValues,
+            function (data) {
+                alert("Data: " + data);
+            }
+        );
+
     });
 });
