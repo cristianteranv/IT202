03b26259d383ce35e8cec05f5170484881663c25
Cristian Teran <cristianteranv@gmail.com>
2020-07-31 22:50:37 -0400

    Implementing adding to cart functionality

--- public_html/Project/addToCart.php
+++ public_html/Project/addToCart.php
@@ -3,23 +3,18 @@
     require("common.inc.php");
     $db = getDB();
     if (isset($_POST["userId"]) && isset($_POST["productId"]) && isset($_POST["purchaseQuantity"]) && isset($_POST["price"])) {
-        try {
-            $userId = $_POST["userId"];
-            $productId = $_POST["productId"];
-            $quantity = $_POST["purchaseQuantity"];
-            $price = $_POST["price"];
-            $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
-            $result = $stmt->execute(array(
-                ":userId" => $userId,
-                ":productId" => $productId,
-                ":quantity" => $quantity,
-                ":price" => $price
-            ));
-            echo "success";
-        }
-        catch (Exception $e){
-            echo $e->getMessage();
-        }
+        $userId = $_POST["userId"];
+        $productId = $_POST["productId"];
+        $quantity = $_POST["purchaseQuantity"];
+        $price = $_POST["price"];
+        $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
+        $result = $stmt->execute(array(
+            ":userId" => $userId,
+            ":productId" => $productId,
+            ":quantity" => $quantity,
+            ":price" => $price
+        ));
+        echo "success";
     }
     else{
         echo "Error while receiving data.";
