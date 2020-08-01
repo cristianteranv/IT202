af15683b96775e5c14b082f9409ca96a745e4085
Cristian Teran <cristianteranv@gmail.com>
2020-08-01 17:10:50 -0400

    Implementing adding to cart functionality

--- public_html/Project/addToCart.php
+++ public_html/Project/addToCart.php
@@ -12,12 +12,51 @@
                 ":userId" => $userId,
                 ":productId" => $productId
             ));
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            if($result){
-                echo "\$result is true\n" . var_export($result);
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo "something went wrong";
             }
             else{
-                echo "\$result is false\n" .  var_export($result);
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                if($result){
+                    /*      we should update purchase quantity of row     */
+                    $cartsId = $result["id"];
+                    $quantity = $quantity + $result["quantity"];
+                    $stmt = $db->prepare("UPDATE Carts SET quantity = :quantity WHERE id = :id");
+                    $stmt->execute(array(
+                        ":quantity" => $quantity,
+                        ":id" => $cartsId
+                    ));
+                    $ee = $stmt->errorInfo();
+                    if($ee[0] != "00000"){
+                        echo "error when updating quantity";
+                    }
+                    else{
+                        echo "success updating quantity";
+                    }
+                }
+                else{
+                    /*    We should create new row     */
+                    $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
+                    $result = $stmt->execute(array(
+                        ":userId" => $userId,
+                        ":productId" => $productId,
+                        ":quantity" => $quantity,
+                        ":price" => $price
+                    ));
+                    $e = $stmt->errorInfo();
+                    if ($e[0] != "00000") {
+                        echo var_export($e, true);
+                    }
+                    else {
+                        echo var_export($result, true);
+                        if ($result) {
+                            echo "Successfully added to cart!";
+                        } else {
+                            echo "Error while adding to cart";
+                        }
+                    }
+                }
             }
 /*
             $stmt = $db->prepare("INSERT INTO Carts(userId, productId, quantity, price) VALUES (:userId, :productId, :quantity, :price)");
