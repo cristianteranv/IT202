79a708cedb3e4eea3b0ba0a71eaa42ce79a5518c
Cristian Teran <cristianteranv@gmail.com>
2020-08-02 17:23:16 -0400

    Implementing adding to cart functionality

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -3,28 +3,47 @@
     require_once("common.inc.php");
     echo "<title>Your cart</title><h1>Cart Items</h1>";
     if(get($_SESSION,"user",false)){
-        echo "<div>You are logged in</div>";
+        try{
+            $stmt = getDB()->prepare("SELECT p.name, c.price, c.quantity, c.quantity*c.price as total, c.created FROM Carts c, Products p WHERE c.userId = :userId AND c.productId = p.id ORDER BY c.created");
+            $userId = $_SESSION["user"]["id"];
+            $stmt->execute(array(":userId" => $userId));
+            $total = 0;
+            //total = SELECT SUM(price*quantity) as total FROM Carts WHERE userId = :userId
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+            <?php if(isset($results) && count($results) > 0):?>
+                <ul class="itemList">
+                    <li class="itemHeader">
+                        <a class="itemCell">Name</a>
+                        <a class="itemCell">Price</a>
+                        <a class="itemCell">Quantity</a>
+                        <a class="itemCell">Total price</a>
+                    </li>
+            <?php foreach($results as $row):?>
+                    <li class="itemListing">
+                        <a class="itemCell" style="width: 50px"><?php echo get($row, "name")?></a>
+                        <a class="itemCell" style="width: 100px"><?php echo get($row, "price")?></a>
+                        <a class="itemCell" style="width: 100px"><?php echo get($row, "quantity")?></a>
+                        <a class="itemCell" style="width: 100px"><?php echo get($row, "total"); $total = $total + get($row, "total");?></a>
+                        <form method="post" class="editQuantity">
+                            <input type="number" name="quantity" value="<?php echo get($row, "quantity")?>" min="1" style="width: 30%">
+                            <input type="submit" id="subQuantityEdit" value="Edit quantity" style="width: 70%; padding: 3px; margin: 2px;">
+                        </form>
+                        <form method="post" class="editQuantity">
+                            <input type="number" name="quantity" value="0" hidden>
+                            <input type="submit" id="removeItem" value="Remove item">
+                        </form>
+                    </li>
+            <?php endforeach;?>
+                </ul>
+            <?php else:
+                echo "<div>Your cart is empty, time to do some shopping!</div>";
+            endif;
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
     }
     else{
         alert("You need to log in");
         header("Location: login.php");
-    }
-    /*$userId = $_SESSION["user"]["id"];*
-    $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM Carts WHERE userId = :userId");
-    $stmt->execute(array("userId"=>$userId));
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if(!$results){
-        echo "<div>Your cart is empty, time to do some shopping!</div>";
-    }
-    else{
-        echo "your items go here";
-    }/
-/*if empty
-your cart is empty :(
-else
-here are the items in your cart:
-  list
-purchase items (remove items from cart, add to orders table)*/
-?>
-
+    }?>
