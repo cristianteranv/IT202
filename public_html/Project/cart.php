96c93563491f312002763d1bf8ee934d4d8b93e6
Cristian Teran <cristianteranv@gmail.com>
2020-08-01 21:08:09 -0400

    Implementing adding to cart functionality

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -1,7 +1,12 @@
 <?php
     include("header.php");
     echo "<title>Your cart</title><h1>Cart Items</h1>";
-    is_logged_in(true);
+    if(!isset($_SESSION["user"])){
+        echo "<div>You are not logged in</div>";
+    }
+    else{
+        echo "<div>You are logged in</div>";
+    }
     //if empty
     // your cart is empty :(
     //else
