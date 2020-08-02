3de9e7d620d9660c89b666f83f8516bc2b122b4d
Cristian Teran <cristianteranv@gmail.com>
2020-08-01 23:43:02 -0400

    Implementing adding to cart functionality

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -9,19 +9,23 @@
         alert("You need to log in to see your cart.");
         header("login.php");
     }
-    /*if(!isset($_SESSION["user"])){
-        echo "<div>you are not logged in</div>";
-        flash("You must log in to see your cart.");
-        getFlashMessages();
-        header("login.php");
+    $userId = $_SESSION["user"]["id"];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT * FROM Carts WHERE userId = :userId");
+    $stmt->execute(array("userId"=>$userId));
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    if(!$results){
+        echo "<div>Your cart is empty, time to do some shopping!</div>";
     }
-    else{
-        echo "<div>You are logged in</div>";
-    }*/
+    else{?>
+        <ul><li><a>some item</a></li></ul>;
+<?php
+    }
+    echo "<div>it works</div>";
+?>
     //if empty
     // your cart is empty :(
     //else
     //  here are the items in your cart:
     //      list
     //  purchase items (remove items from cart, add to orders table)
-?>
